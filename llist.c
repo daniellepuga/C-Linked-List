9f65665915bb674de9ec51da954d3132bb56729e
Danielle Puga <danielle.puga@gmail.com>
2022-02-05 16:03:22 -0800

    added empty functions

--- llist.c
+++ llist.c
@@ -1,2 +1,36 @@
-#include <llist.h>
+#include "llist.h"
 
+void llist_insert_head(struct node **head, struct node *n)
+{
+
+}
+
+struct node *llist_delete_head(struct node **head)
+{
+
+}
+
+void llist_insert_tail(struct node **head, struct node *n)
+{
+
+}
+
+void llist_print(struct node *head)
+{
+
+}
+
+void llist_free(struct node **head)
+{
+
+}
+
+struct node *node_alloc(int value)
+{
+
+}
+
+void node_free(struct node *n)
+{
+
+}
